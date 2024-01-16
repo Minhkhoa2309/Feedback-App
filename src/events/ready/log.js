e2e246d14b655a8efdffe3edb63d532301c3f817
Minhkhoa2309 <63503994+Minhkhoa2309@users.noreply.github.com>
2024-01-16 10:00:18 +0700

    Split Discord command event to second application

--- src/events/ready/log.js
+++
@@ -1,5 +0,0 @@
-/** @param {import('discord.js').Client<true>} client */
-
-module.exports = (client) => {
-    console.log(`${client.user.tag} is online`);
-}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 const createServer = require('./utils/server');
-const setupDiscordBot = require('./utils/setupDiscordBot');
 
 // Start Express server
 const app = createServer();
@@ -11,7 +10,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
-    setupDiscordBot()
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connect to DB.')
 });
